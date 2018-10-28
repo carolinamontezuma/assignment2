eced4b02508636cc7e7594524852a29df09bb5bb
Carolina Montezuma <carolina.leite.montezuma@gmail.com>
2018-10-28 01:16:03 +0000

    add classes e dtos

---
+++ project2-code-parent/project2-code-parent/project2-code-business/src/main/java/ejb/ContentEJB.java
@@ -0,0 +1,20 @@
+package ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ContentEJB
+ */
+@Stateless
+@LocalBean
+public class ContentEJB implements ContentEJBRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public ContentEJB() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
