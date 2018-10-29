5701c95a86c4bd5291095636ee92ad930be05366
Carolina Montezuma <carolina.leite.montezuma@gmail.com>
2018-10-29 02:58:23 +0000

    metodos contentejb

---
+++ project2-code-parent/project2-code-business/src/main/java/dto/ContentDTO.java
@@ -0,0 +1,54 @@
+package dto;
+
+import java.io.Serializable;
+
+public class ContentDTO implements Serializable{
+	int id;
+	 private String title;
+	 private String director;
+	 private int year;
+	 private String category;
+	 
+	 
+	 public ContentDTO() {
+	  super();
+	 }
+	 
+	 public ContentDTO(String title, String director, int year, String category) {
+	  super();
+	  this.title = title;
+	  this.director = director;
+	  this.year = year;
+	  this.category=category;
+	 }
+
+	 public String getTitle() {
+	  return title;
+	 }
+	 public void setTitle(String title) {
+	  this.title = title;
+	 }
+	 public String Director() {
+	  return director;
+	 }
+	 public void setDirector(String director) {
+	  this.director = director;
+	 }
+	 public int getYear() {
+	  return year;
+	 }
+	 public void setYear(int year) {
+		  this.year=year;
+	 }
+	 public String getCategory() {
+		  return category;
+		 }
+	 public void setCategory(String category) {
+		 this.category=category;
+	 }
+	 
+	 @Override
+	 public String toString() {
+	  return " title = " +  this.title + " director = " + this.director + " year = " + this.year + "category = "+this.category;
+	 }
+}
