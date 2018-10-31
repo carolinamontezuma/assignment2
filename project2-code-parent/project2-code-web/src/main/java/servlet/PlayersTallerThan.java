04ad23f26c4adac205838cd06c52557f6468ea53
peixaxo <joaoa13portugal@gmail.com>
2018-10-31 17:01:08 +0000

    commit cenas

--- project2-code-parent/project2-code-web/src/main/java/servlet/PlayersTallerThan.java
+++ project2-code-parent/project2-code-web/src/main/java/servlet/PlayersTallerThan.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,7 +22,7 @@ import ejb.ContentEJBRemote;
 //http://localhost:8080/project2-code-web/Webflix?fill=1
 //url = http://localhost:8080/project2-code-web/Webflix?category=comedy
 
-@WebServlet("/Webflix")
+@WebServlet("/PlayersTallerThan")
 public class PlayersTallerThan extends HttpServlet {
  private static final long serialVersionUID = 1L;
  @EJB
@@ -39,57 +40,33 @@ public class PlayersTallerThan extends HttpServlet {
   */
  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	 PrintWriter out = response.getWriter();
-  response.setContentType("text/html");
-/*
-  if (request.getParameter("fill") != null) {
-   ejbremote.populate();
-   out.println("<h1>Populate Content: OK!</h1>");
-  }
-  else if(request.getParameterValues("range") != null) {
-	  List<ContentDTO> content =  ejbremote.seeContentFromYears(1997, 2001);
-	   out.println("<h1> Contents </h1>");
-	   for (ContentDTO p : content)
-	    out.println(p + "<br/>");
+	 response.setContentType("text/html");
+	 //out.println("<h1> PAI NATAL </h1>");
 
-  }
-  else if(request.getParameterValues("all") != null) {
-	  List<ContentDTO> content =  ejbremote.seeAllContent();
-	   out.println("<h1> Contents </h1>");
-	   for (ContentDTO p : content)
-	    out.println(p + "<br/>");
+	 if (request.getParameter("fill") != null) {
+		 ejbremote.populate();
+		 out.println("<h1>Populate Content: OK!</h1>");
+	 }
+ 
 
-  }
-  else if(request.getParameterValues("director") != null) {
-	  List<ContentDTO> content =  ejbremote.seeContentFromDirector("John");
-	   out.println("<h1> Contents </h1>");
-	   for (ContentDTO p : content)
-	    out.println(p + "<br/>");
+   if(request.getParameter("director") != null) {
+    	 String directorName = request.getParameter("director");
+        // String addressPath = "/listContents.jsp";
+         //RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("listContents.jsp");
 
-  }
-  else {
-   String category = request.getParameter("category");
-   List<ContentDTO> content = ejbremote.seeContentFromCategory("comedy");
+    	   // out.println(directorName + "<br/>");
+    	 List<ContentDTO> content =  ejbremote.seeContentFromDirector(directorName);
+         request.setAttribute("listDirector", content);
+         RequestDispatcher dispatcher = request.getRequestDispatcher("/listContents.jsp");
 
-   out.println("<h1> Contents </h1>");
-   for (ContentDTO p : content)
-    out.println(p + "<br/>");
-  }
-
-*/
-
-     String productName = request.getParameter("director");
-	   out.println(productName);
-
-     /*String addressPath = "/listContents.jsp";
-     RequestDispatcher dispatcher = request.getRequestDispatcher(addressPath);
-     dispatcher.forward(request, response);*/
-     if(request.getParameterValues("director") != null) {
-   	  List<ContentDTO> content =  ejbremote.seeContentFromDirector(productName);
-   	   out.println("<h1> Contents </h1>");
-   	   for (ContentDTO p : content)
-   	    out.println(p + "<br/>");
+        dispatcher.forward(request, response);
+  		//
+   	  	
+   	  /* for (ContentDTO p : content)
+   	    out.println(p + "<br/>");*/
 
      }
+ 
     
  }
 
@@ -99,6 +76,7 @@ public class PlayersTallerThan extends HttpServlet {
   */
  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	 doGet(request, response);
+	     
 
  }
 }
